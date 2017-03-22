f2418269a01b68186c3c627d4c48dbb245ad9bad
Brendan O'Brien <sparkle_pony_2000@qri.io>
2017-03-22 19:47:27 -0400

    turns out we need those last_get != ''s

--- queries.go
+++ queries.go
@@ -9,6 +9,7 @@ from
 where 
   url ilike $1
   and content_sniff != 'text/html; charset=utf-8'
+  and last_get is not null
   -- confirm is not empty hash
   and hash != '1220e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'
   and not exists (select null from metadata where urls.hash = metadata.subject) 
@@ -23,6 +24,7 @@ from
 where 
   url ilike $1
   and content_sniff != 'text/html; charset=utf-8'
+  and last_get is not null
   -- confirm is not empty hash
   and hash != '1220e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'
   and exists (select null from metadata where urls.hash = metadata.subject) 
